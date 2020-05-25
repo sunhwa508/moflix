4a8a604a17f91683fc44aeeac15dd9e0187f86bc
sunhwa508 <2017sunhwa@gmail.com>
2020-05-25 23:16:53 +0900

    Updates

---
+++ precache-manifest.71e3c9e5c5b1f237935495b835549f9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db303d951e399887a59b75f981ca4a0",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "02839fc8d15a13a6b43f",
+    "url": "/movie/static/css/main.6de28776.chunk.css"
+  },
+  {
+    "revision": "3bfb1e2ca75bca09dc4e",
+    "url": "/movie/static/js/2.c19f1243.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie/static/js/2.c19f1243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02839fc8d15a13a6b43f",
+    "url": "/movie/static/js/main.d01e1f01.chunk.js"
+  },
+  {
+    "revision": "a9664e937f0daa8fb643",
+    "url": "/movie/static/js/runtime-main.d9fbd007.js"
+  }
+]);
